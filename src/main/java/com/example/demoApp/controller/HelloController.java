4352a0777008f621d34bbf2c4f07cbe81e26b348
Aditya Gupta <ag7023587@gmail.com>
2025-03-01 15:18:26 +0530

    UC2 added

--- src/main/java/com/example/demoApp/controller/HelloController.java
+++ src/main/java/com/example/demoApp/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.demoApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-    @RequestMapping("/")
-    public String getMessage(){
-        return "Welcome to Spring Boot!!";
+    @RequestMapping(value={"/query"},method= RequestMethod.GET)
+    public String getMessage(@RequestParam(value="name") String name){
+
+        return "Hello"+ name+ "!";
     }
 }
+
